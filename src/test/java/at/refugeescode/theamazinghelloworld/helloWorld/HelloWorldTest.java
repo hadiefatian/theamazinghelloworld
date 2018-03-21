d3fa09af40a1130e9635d0d3cd4989355ff83f05
Hadi <hadi.effatian@gmail.com>
2018-03-21 18:46:59 +0100

    Implimented test

---
+++ src/test/java/at/refugeescode/theamazinghelloworld/helloWorld/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package at.refugeescode.theamazinghelloworld.helloWorld;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class HelloWorldTest {
+
+    @Autowired
+    private HelloWorld helloWorld;
+
+    @Test
+    void sayHelloWorld() {
+
+        String message = helloWorld.sayHelloWorld();
+        Assertions.assertEquals("Hello World", message);
+    }
+}
